3f9a98ebfa00eaefe7a45ff3827c3d3dfd91f0de
Jakub Kusz <jakub.kusz@onet.pl>
2023-06-21 11:41:25 +0200

    modyfikacja Base tak, aby wiadomo było co buduje

--- inc/Base.hpp
+++ inc/Base.hpp
@@ -14,10 +14,13 @@ class Base: public Unit{
         static constexpr int _building_time = 0;    //!< default building time
 
         static constexpr Type_of_unit _unit = Type_of_unit::BASE; //!< unit type
+        bool under_construction = false; //!< if true Base builds the unit
+        Type_of_unit unit_under_construction; //!< unit under construction
 
     public:
     /*Method descriptions can be found in the corresponding source file */
 
+        Base(int _id, Ownership _ownership, Type_of_unit _unit_under_construction);
         Base(int _id, Ownership _ownership);
         ~Base();
 };
