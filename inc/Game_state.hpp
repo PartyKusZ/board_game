b4f686c0e893b42ca64ebc68bb6af04203f377f3
PartyKusZ <jakub.kusz@onet.pl>
2023-06-22 17:23:09 +0200

    utworzenie soursa GAme state i podstawowch definifcji fubkcji

--- inc/Game_state.hpp
+++ inc/Game_state.hpp
@@ -5,4 +5,9 @@ class Game_state{
     public:
         long gold_amount;
         Map_table map;
+        Game_state();
+        void find_my_base();
+        void find_enemies_base();
+        /*zastanowic sie jakie metody przeszukiwania mapy należy zaimplementować*/
+        ~Game_state();
 };
---
+++ src/Game_state.cpp
@@ -0,0 +1,6 @@
+#include "Game_state.hpp"
+
+Game_state::Game_state(){};
+
+
+Game_state::~Game_state(){};
